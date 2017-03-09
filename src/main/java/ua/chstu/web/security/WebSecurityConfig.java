9525788049f1492877deca862e232dfb8040c444
esdth <keepass.log@gmail.com>
2017-03-09 13:07:05 +0200

    Configured CORS for security

--- src/main/java/ua/chstu/web/security/WebSecurityConfig.java
+++ src/main/java/ua/chstu/web/security/WebSecurityConfig.java
@@ -3,6 +3,8 @@ package ua.chstu.web.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -12,6 +14,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 
 @Configuration
 @EnableWebSecurity
+@Order(-1)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
@@ -22,8 +25,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
             .authorizeRequests()
-            .antMatchers("/question/all").authenticated()
-            .antMatchers("/user/").permitAll()
+                .antMatchers(HttpMethod.OPTIONS,"/**").permitAll()
+                .antMatchers("/question/all").authenticated()
+                .antMatchers("/user/").permitAll()
             .and()
             .httpBasic()
             .and()
