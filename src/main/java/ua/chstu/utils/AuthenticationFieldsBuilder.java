76ea10fc76c62d35ef226d1f5feef75de59879a7
MaxymHybalo <esdth.mo@gmail.com>
2017-06-04 19:20:29 +0300

    Builder for latin fields (e.g. login, password)

---
+++ src/main/java/ua/chstu/utils/AuthenticationFieldsBuilder.java
@@ -0,0 +1,28 @@
+package ua.chstu.utils;
+
+import java.util.Random;
+
+public class AuthenticationFieldsBuilder {
+
+
+    public String buildField(){
+        int numericPart = (int) Math.round(Math.random() * 100);
+        return randomStringPart() + numericPart;
+    }
+
+    public String randomStringPart(){
+        StringBuilder s = new StringBuilder();
+        for (int i = 0; i < 4; i++) {
+            s.append(Character.toString(randomChar()));
+        }
+        return s.toString();
+    }
+
+    public char randomChar(){
+        Random r = new Random();
+        char result = (char) (r.nextInt(26)+97);
+        System.out.println(result);
+        return result;
+
+    }
+}
--- src/test/java/ua/chstu/StudQuizApplicationTests.java
+++ src/test/java/ua/chstu/StudQuizApplicationTests.java
@@ -1,16 +1,31 @@
 package ua.chstu;
 
+import org.junit.Assert;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+import ua.chstu.utils.AuthenticationFieldsBuilder;
+
 public class StudQuizApplicationTests {
 
+	AuthenticationFieldsBuilder afb;
+
 	@Test
-	public void contextLoads() {
+	public void testRandomChar(){
+		afb = new AuthenticationFieldsBuilder();
+		Assert.assertTrue(afb.randomChar() != 0);
 	}
 
+	@Test
+	public void testStringPart(){
+		afb = new AuthenticationFieldsBuilder();
+		Assert.assertTrue(afb.randomStringPart().length() == 4);
+	}
+
+	@Test
+	public void testFullField(){
+		afb = new AuthenticationFieldsBuilder();
+		String field = afb.buildField();
+		System.out.println(field);
+		Assert.assertTrue(field.length() > 4);
+	}
 }
